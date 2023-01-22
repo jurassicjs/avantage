840e7f6e0036de7b94d24c6892a35bd1578ea2d2
Rohrig <rick@wambo.com>
2023-01-22 18:32:34 +0100

    update emailSender

--- server/app/email/emailSender.ts
+++ server/app/email/emailSender.ts
@@ -2,25 +2,23 @@
 import { useMailer } from '#mailer'
 import { EmailTemplate } from './types/emailTypes'
 
-type SendMail = {template: EmailTemplate , to: string, from: string,  subject: string}
+type SendMail = { template: EmailTemplate, to: string, fromEmail: string, fromName: string, subject: string }
 export async function sendEmail(request: SendMail) {
   const mailService = useMailer()
-  const gmailTransporter = mailService.gmailTransporter()
 
-  const runtimeConfig = useRuntimeConfig()
-
-  console.log('mailerUser', runtimeConfig.mailerUser)
+  console.log('mail request', request)
 
   await mailService.sendMail({
     requestId: 'test-key',
     options: {
+      fromEmail: request.fromEmail,
+      fromName: request.fromName,
       to: request.to,
       subject: request.subject,
       text: request.template.text,
       html: request.template.html
-    },
-    transporter: gmailTransporter
+    }
   })
 
-  return 
+  return
 }
--- server/app/email/verifyEmail.ts
+++ server/app/email/verifyEmail.ts
@@ -5,5 +5,5 @@ import { createOtp } from '../services/otp'
 export default async function sendVerificationEmail(email: string, userId: number) {
   const otp = await createOtp(userId)
   const template = verifyEmailTemplate(otp, 'support@fullstackjack.dev', 'Avantage Support', 'Avantage')
-  sendEmail({template, to: email, fromEmail: 'jack@fullstackjack.dev', fromName: "Jack", subject:'Avantage email verification'})
+  sendEmail({ template, to: email, fromEmail: 'nuxt-mailer@fullstackjack.dev', fromName: 'Nuxt Mailer', subject: 'Nuxt Mailer email verification' })
 }
