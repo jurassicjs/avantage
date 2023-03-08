181c864c6c6bccf7299fd32fedd28aadd09d25c1
rohrig <richard.t.rohrig@gmail.com>
2023-03-08 21:02:45 +0100

    feature/scheudler comment out task in exampleScheduler

--- server/plugins/exampleScheduler.ts
+++ server/plugins/exampleScheduler.ts
@@ -7,9 +7,9 @@ export default defineNitroPlugin(() => {
 function startScheduler() {
   const scheduler = useScheduler();
 
-  scheduler.run(() => {
-    console.log("cool beans! I run once every second! 😀");
-  }).everySecond();
+  // scheduler.run(() => {
+  //   console.log("cool beans! I run once every second! 😀");
+  // }).everySecond();
 
   // create as many tasks as you want here
 }
