c44eadd29e555e2c78607826185322c3f5626ddc
rohrig <richard.t.rohrig@gmail.com>
2023-03-08 21:27:23 +0100

    feature(scheduler) extract AvantageExample

---
+++ components/AvantageExample.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import TheFeatureGrid from "~~/components/elements/TheFeatureMapGrid.vue";
+import TheBillboard from "~~/components/elements/TheBillboard.vue";
+</script>
+
+<template>
+  <div class="">
+    <div class="py-12 theme-mode dark:bg-black">
+      <div class="max-w-9xl mx-auto px-4 sm:px-6 lg:px-8">
+        <div class="text-center md:flex align-center md:justify-center  ">
+          <TheBillboard />
+        </div>
+      </div>
+    </div>
+    <div class="flex justify-center dark:bg-gray-900 bg-gray-200">
+      <TheFeatureGrid />
+    </div>
+  </div>
+</template>
